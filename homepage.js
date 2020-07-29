69d361fbe6793842acd9af6731622dfbe2be2d99
Shubhamj08 <46774429+Shubhamj08@users.noreply.github.com>
2020-07-29 11:40:59 +0530

    initial commit

---
+++ homepage.js
@@ -0,0 +1,38 @@
+// console.log(document.documentElement.scrollTop || document.body.scrollTop);
+
+// const navbar = document.getElementsByClassName("navbar");
+// const logo = document.getElementById("logo");
+// // const linkText = document.getElementsByClassName("nav-link");
+// // const dropdown = document.getElementsByClassName("dropdown");
+// // const dropContent = document.getElementsByClassName("dropdown-content");
+
+// setInterval(function () {
+//   if (
+//     document.documentElement.scrollTop >= 340 ||
+//     document.body.scrollTop > 340
+//   ) {
+//     navbar[0].style.boxShadow =
+//       "0 5px 10px rgba(0, 0, 0, 0.19), 0 3px 3px rgba(0, 0, 0, 0.23)";
+//     navbar[0].style.background = "#fff";
+//     logo.style.display = "block";
+
+//     // dropdown[0].onmouseover = () => {
+//     //   dropContent[0].style.background = "#888888";
+//     // };
+//     // for (let i = 0; i < linkText.length; i++) {
+//     //   console.log(linkText[i]);
+//     //   linkText[i].style.color = "#000";
+//     // }
+//   } else {
+//     navbar[0].style.boxShadow = "none";
+//     navbar[0].style.background = "transparent";
+//     logo.style.display = "none";
+//     // dropdown[0].onmouseover = () => {
+//     //   dropContent[0].style.background = "transparent";
+//     // };
+//     // for (let i = 0; i < linkText.length; i++) {
+//     //   console.log(linkText[i]);
+//     //   linkText[i].style.color = "#fff";
+//     // }
+//   }
+// }, 200);
